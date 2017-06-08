dcbb60b35900db81451e196e40169ada629c8c41
Emily Boucher <emi.boucher@gmail.com>
2017-06-08 17:18:21 -0400

    added kittens route and controller, DRY cleanup with before action filter added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/welcome' =>     'pages#welcome'
-  get '/contest' =>     'pages#contest'
-  get 'kitten/:size' => 'pages#kitten'
+  get '/welcome' =>       'pages#welcome'
+  get '/contest' =>       'pages#contest'
+  get 'kitten/:size' =>   'pages#kitten'
+  get 'kittens/:size' =>  'pages#kittens'
 end
