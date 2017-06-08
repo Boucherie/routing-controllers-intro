1b97bb554e5e8f14e5842d358c7a432c0a9db7d3
Emily Boucher <emi.boucher@gmail.com>
2017-06-08 17:00:42 -0400

    added dynamic route for kittens, working; found some cats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,16 @@
 class PagesController < ApplicationController
+
+  def welcome
+    @header = "SPARTA!!"
+  end
+
+  def contest
+    render plain: "Contest opens soon!"
+  end
+
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
 end
