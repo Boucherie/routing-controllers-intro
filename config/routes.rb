846216a2af4c19557faa1be450eb767c56cfdcbb
Emily Boucher <emi.boucher@gmail.com>
2017-06-08 22:48:10 -0400

    route helper section completed, route prefixes appended

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root 'pages#welcome'
   get '/welcome' =>       'pages#welcome'
   get '/contest' =>       'pages#contest'
-  get '/kitten/:size' =>   'pages#kitten'
-  get '/kittens/:size' =>  'pages#kittens'
-  get '/secrets/:magic_word' => 'pages#secrets'
+  get '/kitten/:size' =>   'pages#kitten', as: 'kitten'
+  get '/kittens/:size' =>  'pages#kittens', as: 'clowder'
+  get '/secrets/:magic_word' => 'pages#secrets', as: 'secrets'
 end
